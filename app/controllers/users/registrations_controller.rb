b9b5640ae543c8db460c36730e8594b3fac4ba74
yoshimura <kyosimura23@gmail.com>
2019-08-06 23:42:05 +0900

    add gem omniauth-rails_csrf_protection

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  def after_update_path_for(_resource)
-    user_path(id: current_user.id)
-  end
 
   def build_resource(hash = {})
     hash[:uid] = User.create_unique_string
