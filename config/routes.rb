0dc56575fb846fe3d68765f56acaba24cba0eec2
yoshimura <kyosimura23@gmail.com>
2019-08-09 21:41:27 +0900

    config omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "oauth/index"
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   resources :users, only: %i[show index]
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   resources :books do
     resources :comments
@@ -17,7 +20,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root to: "oauth#index"
 
   resources :users do
     member do
